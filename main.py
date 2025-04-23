3ad2cb54fa336a726ff97370cc1b3e4aa9be9285
portilho13 <junior.portilho2005@gmail.com>
2025-04-23 16:19:09 +0100

    created parse exception

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 from fm import FileManager
 import base64
+from parser.pe import ParseError
 
 def main():
-    fm = FileManager()
-    tables = fm.LoadTables()
+    #fm = FileManager()
+    #tables = fm.LoadTables()
     
-    t = tables[0]
+    #t = tables[0]
     #t.printTable()
 
-    t.getByParams(["Id"])
+    #t.getByParams(["Id"])
+
+    e = ParseError(13, 'Expected "{" but found "%s"', "[")
+    print(e)
 
 
 
---
+++ parser/pe.py
@@ -0,0 +1,8 @@
+class ParseError(Exception):
+    def __init__(self, pos, msg, *args):
+        self.pos = pos
+        self.msg = msg
+        self.args = args
+
+    def __str__(self):
+        return '%s at postion %s' % (self.msg % self.args, self.pos)
