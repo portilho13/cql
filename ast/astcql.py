94d76b5ad7023b11876eca61e70741099ffa7fa3
portilho13 <junior.portilho2005@gmail.com>
2025-04-25 19:31:17 +0100

    removed astcql node self.type as is redundant

--- ast/astcql.py
+++ ast/astcql.py
@@ -8,29 +8,24 @@ class ImportTableNode(AstNode):
 
 class ExportTableNode(AstNode):
     def __init__(self, text):
-        self.type = "EXPORT"
         self.table_name = text["identifier"]
         self.destination = text["destination"]
 
 class DiscardTableNode(AstNode):
     def __init__(self, table_name):
-        self.type = "DISCARD"
         self.table_name = table_name
 
 class RenameTableNode(AstNode):
     def __init__(self, old_name, new_name):
-        self.type = "RENAME"
         self.old_name = old_name
         self.new_name = new_name
 
 class PrintTableNode(AstNode):
     def __init__(self, table_name):
-        self.type = "PRINT"
         self.table_name = table_name
 
 class SelectNode(AstNode):
     def __init__(self, sel, from_table_name, optional_condition, limit):
-        self.type = "SELECT"
         self.sel = sel,
         self.from_table_name = from_table_name
         self.optional_condition = optional_condition
